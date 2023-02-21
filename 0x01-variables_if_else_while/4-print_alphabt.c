aaa281172e2ff7f3e7afbfd0b504b844d1aa8b27
kap48 <kadiriallae@gmail.com>
2023-02-20 16:14:49 -0800

    re'

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+*main - Entry point
+*Return: allways null
+*/
+int main(void)
+{
+	char alp[24] = "abcdfghijklmnoprstuvwxyz";
+	int i;
+
+	for (i = 0; i < 24; i++)
+	{
+		putchar(alp[i]);
+	}
+	putchar('\n');
+	return (0);
+}
